8fb03db795a546a391f2f26fc48a3b3ed7f950bd
IKRadwan <146995869+IKRadwan@users.noreply.github.com>
2023-11-27 19:28:48 +0100

    test: add backend block

--- backend.tf
+++ backend.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.0.11"
 
+  backend "gcs" {
+    bucket = "dareit-task8-bucket-st"
+    prefix = "dev"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"

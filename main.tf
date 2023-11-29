665676c9110bf668b44b89ebb8b6f4a011bb0d67
IKRadwan <146995869+IKRadwan@users.noreply.github.com>
2023-11-29 19:13:46 +0100

    feat: create website content by terraform not Linux command

--- main.tf
+++ main.tf
@@ -1,26 +1,32 @@
 resource "google_storage_bucket" "static" {
-  name          = "dareit_task8_bucket"
+  name          = "dareit_task8_terraform"
   location      = "US"
   force_destroy = true
 
   uniform_bucket_level_access = true
 
   website {
-    main_page_suffix = "./website/index.html"
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
   }
 }
 
-/*
-resource "google_storage_bucket_object" "default" {
-  name   = "index.html"
-  source = "index.html"
-  bucket = google_storage_bucket.static.id
-}
-*/
-
 resource "google_storage_bucket_iam_member" "member" {
   provider = google
-  bucket   = google_storage_bucket.static.id
+  bucket   = "dareit_task8_terraform"
   role     = "roles/storage.objectViewer"
   member   = "allUsers"
+}
+
+
+resource "google_storage_bucket_object" "static-website" {
+  name   = "index.html"
+  source = "./website/index.html"
+  bucket = "dareit_task8_terraform"
+}
+
+resource "google_storage_bucket_object" "picture" {
+  name   = "myimage.png"
+  source = "./website/images/myimage.png"
+  bucket = "dareit_task8_terraform"
 }
